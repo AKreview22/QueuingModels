b1cf5866e9c7028718f9fd0dde73ca9747b3cd26
Ahmed Kamal <AKreview22@gmail.com>
2020-04-27 02:21:01 +0200

    main fix

--- src/queue/Main.java
+++ src/queue/Main.java
@@ -6,7 +6,9 @@ public class Main {
 
 
     public static void main(String[] args) {
-        /*queue.MD1 test = new queue.MD1() ;
+
+        //test
+        MD1 test = new MD1();
         test.setArrivalsMean(50);
         System.out.println("lambda : " + test.getArrivalsMean());
         test.setUnitsMean(70);
@@ -18,7 +20,7 @@ public class Main {
         test.CalculateLq();
         System.out.println(" Lq is :" + test.getLq());
         test.CalculateWq();
-        System.out.println(" Wq is :" + test.getWq());*/
+        System.out.println(" Wq is :" + test.getWq());
 
 
        /* MM1 test = new MM1();
